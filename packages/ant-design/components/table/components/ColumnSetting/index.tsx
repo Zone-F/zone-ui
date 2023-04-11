de99834cff166fcb259978318a7a97c0b91ccda8
zone98f <1191066968@qq.com>
2023-04-11 23:00:43 +0800

    新增测试用例

--- packages/ant-design/components/table/components/ColumnSetting/index.tsx
+++ packages/ant-design/components/table/components/ColumnSetting/index.tsx
@@ -1,8 +1,8 @@
 import { SettingOutlined, AudioOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Divider, Space, Table, TableColumnType } from 'antd';
 import type { ProColumnsType } from 'table-render';
-import { Popover, Input, Tooltip } from 'antd';
-import classNames from 'classNames'
+ * @ Modified by: zone98f
+ * @ Modified time: 2023-04-11 22:55:20
 import React, { memo, useContext, useEffect, useMemo, useState } from 'react';
 import { TableContext } from '../../Store/Provide';
 import { ColumnsState } from '../../typing';
--- packages/ant-design/components/table/components/ColumnSetting/indexColumnSetting.cy.tsx
+++ packages/ant-design/components/table/components/ColumnSetting/indexColumnSetting.cy.tsx
@@ -1,11 +1,47 @@
+/**
+ * @ Author: zone98f
+ * @ Create Time: 2023-04-11 20:06:58
+ * @ Modified by: zone98f
+ * @ Modified time: 2023-04-11 22:55:42
+ * @ Description: 表格列设置组件
+ * -弹窗开启
+ * -弹窗关闭
+ * -组件渲染列表的初始值
+ * -搜索框搜索关键词
+ * -搜索框清空关键词
+ * -点击子项的复选框
+ */
+
 import React from 'react'
 import ColumnSetting from './index'
 
 describe('<ColumnSetting />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  it('render', () => {
     cy.mount(<ColumnSetting />)
+    // 
+    cy.get('span').should('have.text', '0')
+  })
+  it('Tooltip Title', () => {
+    cy.mount(<ColumnSetting />)
+    // 
+    cy.get('span').should('have.text', '0')
+  })
+  it('columnSetting open checkable', () => {
+    cy.mount(<ColumnSetting />)
+
+    cy.get('span').should('have.text', '0')
+  })
+  it('columnSetting close checkable', () => { })
+  it('columnSetting render list check', () => {
+    // see: https://on.cypress.io/mounting-react
+    const defaultColumns = [];
+
+    cy.mount(<ColumnSetting />)
+
     cy.get('span').should('have.text', '0')
 
   })
+  it('search', () => { })
+  it('reset search', () => { })
+  it('columnSetting select one', () => { })
 })
