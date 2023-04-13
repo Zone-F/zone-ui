fb0e4a93b2d49109059559de402c0d7a5ef6e5cd
Zone-F <1191066968@qq.com>
2023-04-13 14:39:13 +0800

    新增测试命令

--- packages/ant-design/cypress.config.ts
+++ packages/ant-design/cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  "video": false,
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
     },
+    specPattern:"components/**/*.cy.{js,jsx,ts,tsx}"
   },
-  e2e: {},
 });
